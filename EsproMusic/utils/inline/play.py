a946ff0b963c7cda701c4c3c5eb46996ca5fcfa9
ramavtarchauhan420 <ramavtarchauhan420@gmail.com>
2025-05-28 19:36:45 +0530

    Update play.py

--- EsproMusic/utils/inline/play.py
+++ EsproMusic/utils/inline/play.py
@@ -1,7 +1,8 @@
 import math
-
+import random
 from pyrogram.types import InlineKeyboardButton
-
+from EsproMusic import app
+import config
 from EsproMusic.utils.formatters import time_to_seconds
 
 
@@ -53,18 +54,18 @@ def stream_markup_timer(_, chat_id, played, dur):
     else:
         bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·"
     buttons = [
-        [
-           # InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
-            #InlineKeyboardButton(text="➚", callback_data=f"ADMIN Resume|{chat_id}"),
-           # InlineKeyboardButton(text="↺", callback_data=f"ADMIN Replay|{chat_id}"),
-           # InlineKeyboardButton(text="➘", callback_data=f"ADMIN Stop|{chat_id}"),
-           # InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-        ],
-        [
+                [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
+        ],
+                [
+         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
+        ],
+        [
+        # InlineKeyboardButton(text="Update", url="https://t.me/Jani_RP"),
+        # InlineKeyboardButton(text="Support", url="https://t.me/+AaI_GATiYwQ5NjU1"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -74,11 +75,8 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
-           # InlineKeyboardButton(text="➚", callback_data=f"ADMIN Resume|{chat_id}"),
-           # InlineKeyboardButton(text="⎋", callback_data=f"ADMIN Replay|{chat_id}"),
-           # InlineKeyboardButton(text="➘", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+        # InlineKeyboardButton(text="Update", url="https://t.me/Jani_RP"),
+        # InlineKeyboardButton(text="Support ", url="https://t.me/+AaI_GATiYwQ5NjU1"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -90,11 +88,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"LoyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"LoyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -105,6 +103,7 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         ],
     ]
     return buttons
+
 
 
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
@@ -154,3 +153,4 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
+            
